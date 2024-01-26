a4ed135071e1ca2ce73bcffdfc8de8385ed0def4
MiriamEvardi <miriamevardi97@gmail.com>
2024-01-26 18:26:01 +0100

    adding controls (bonus)

--- js/script.js
+++ js/script.js
@@ -6,22 +6,26 @@ let kmPrice = 0.21;
 
 let discountedPrice = kmPrice;
 
+if (!isNaN(age) && 0 < age && 100 > age && Number.isInteger(age) && Number.isInteger(kmToTravel) && !isNaN(kmToTravel) && kmToTravel > 0) {
 
+    if (age < 18) {
+        let discount = 0.2;
 
-if (age < 18) {
-    let discount = 0.2;
+        document.getElementById('discount').innerHTML = "Hai ricevuto uno sconto del 20%";
+        discountedPrice = kmPrice * (1 - discount);
 
-    document.getElementById('discount').innerHTML = "Hai ricevuto uno sconto del 20%";
-    discountedPrice = kmPrice * (1 - discount);
+    } else if (age > 64) {
+        let discount = 0.4;
 
-} else if (age > 64) {
-    let discount = 0.4;
+        document.getElementById('discount').innerHTML = "Hai ricevuto uno sconto del 40%";
+        discountedPrice = kmPrice * (1 - discount);
+    }
 
-    document.getElementById('discount').innerHTML = "Hai ricevuto uno sconto del 40%";
-    discountedPrice = kmPrice * (1 - discount);
+    let totalCost = kmToTravel * discountedPrice;
+    let lastPrice = totalCost.toFixed(2);
+
+    document.getElementById('trainTicket').innerHTML = lastPrice + ' €';
+
+} else {
+    alert("Ricarica la pagina e inserisci un'età valida compresa tra 0 e 100.");
 }
-
-let totalCost = kmToTravel * discountedPrice;
-let lastPrice = totalCost.toFixed(2);
-
-document.getElementById('trainTicket').innerHTML = lastPrice + ' €';
