b18dbbd465f7a5b55a846cb7536e6fa939628b59
MiriamEvardi <miriamevardi97@gmail.com>
2024-01-26 13:56:50 +0100

    first discount

--- js/script.js
+++ js/script.js
@@ -4,10 +4,17 @@ const age = Number(prompt("Indica la tua età"));
 
 let kmPrice = 0.21 * 10;
 
+let discountedPrice = kmPrice;
+
 
 
 if (age < 18) {
     let discount = 0.2;
     document.getElementById('discount').innerHTML = "Hai ricevuto uno sconto del 20%";
-    document.getElementById('trainTicket').innerHTML = kmPrice * discount + '€';
+    discountedPrice = kmPrice * (1 - discount);
 }
+
+let totalCost = kmToTravel * discountedPrice;
+let lastPrice = totalCost.toFixed(2);
+
+document.getElementById('trainTicket').innerHTML = lastPrice + ' €';
