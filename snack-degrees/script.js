3d7c3c8e1bbeef0180f56ccbe19510f44b529452
MiriamEvardi <miriamevardi97@gmail.com>
2024-01-29 12:09:57 +0100

    snack- degrees converter

---
+++ snack-degrees/script.js
@@ -0,0 +1,5 @@
+const temperaturaCelsius = prompt("Inserisci la temperatura in gradi Celsius:");
+
+const temperaturaFahrenheit = (temperaturaCelsius * 9 / 5) + 32;
+
+alert("La temperatura in gradi Fahrenheit è: " + temperaturaFahrenheit + "°F");
